e93cdd7751162738fff0681860d54eafbf6cd46a
Tunahan Karaçoban <77543156+TunahanKaracoban@users.noreply.github.com>
2022-04-18 03:07:12 +0300

    Add files via upload

---
+++ PatikaDotnetCore/BookStore/WebApi/Controllers/BookController.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.BookOperations.CreateBook;
+using WebApi.BookOperations.DeleteBook;
+using WebApi.BookOperations.GetBookDetail;
+using WebApi.BookOperations.GetBooks;
+using WebApi.BookOperations.UpdateBook;
+using WebApi.DBOperations;
+using static WebApi.BookOperations.CreateBook.CreateBookCommand;
+using static WebApi.BookOperations.UpdateBook.UpdateBookCommand;
+
+namespace WebApi.AddControllers
+{
+    [ApiController]
+    [Route("[controller]s")]
+    public class BookController : ControllerBase
+    {
+        private readonly BookStoreDbContext _context;
+        private readonly IMapper _mapper;
+        public BookController(BookStoreDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetBooks()
+        {
+            GetBooksQuery query = new GetBooksQuery(_context,_mapper);
+            var result = query.Handle();
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            BookDetailViewModel result;
+            try
+            {
+                GetBookDetailQuery query = new GetBookDetailQuery(_context,_mapper);
+                query.BookId = id;
+                result = query.Handle();
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult AddBook([FromBody] CreateBookModel newBook)
+        {
+            CreateBookCommand command = new CreateBookCommand(_context,_mapper);
+            try
+            {
+                command.Model = newBook;
+                command.Handle();
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook(int id, [FromBody] UpdateBookModel updatedBook)
+        {
+            try
+            {
+                UpdateBookCommand command = new UpdateBookCommand(_context);
+                command.BookId = id;
+                command.Model = updatedBook;
+                command.Handle();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            try
+            {
+                 DeleteBookCommand command=new DeleteBookCommand(_context);
+                 command.BookId=id;
+                 command.Handle();
+            }
+            catch (Exception ex)
+            { 
+                return BadRequest(ex.Message);
+            }
+            return Ok();
+        }
+    }
+}
